29c23a182c9f29c4a359647341f57f66dda9b0d1
Zack Scoles <zackscoles@gmail.com>
2016-03-22 21:15:48 -0700

    Initial Commit
    Initial Commit

---
+++ JavaScript.js
@@ -0,0 +1,51 @@
+﻿window.onload = function () {
+    var fileInput = document.getElementById('fileInput');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+    fileInput.addEventListener('change', function (e) {
+        var file = fileInput.files[0];
+        var textType = /text.*/.csv;
+
+        if (file.type.match(textType)) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                var result = reader.result;
+
+                var tableData = result.split("\n");
+
+                var myTable = document.createElement("table");
+                var tblBody = document.createElement("tbody");
+
+                for (var i = 0; i < tableData.length; i++) {
+
+                    var row = document.createElement("tr");
+
+                    console.log(tableData[i]);
+
+                    var x = tableData[i].split(", ");
+
+                    for (var a = 0; a < x.length; a++) {
+                        var cell = document.createElement("td");
+                        var cellText = document.createTextNode(x[a]);
+
+                        cell.appendChild(cellText);
+                        row.appendChild(cell);
+
+                        console.log(x[a]);
+                    }
+                    tblBody.appendChild(row);
+                }
+                myTable.appendChild(tblBody);
+
+                myTable.setAttribute("border", "2");
+
+                fileDisplayArea.appendChild(myTable);
+            }
+        reader.readAsText(file);
+
+        } else {
+            fileDisplayArea.innerText = "File not supported!"
+        }
+    });
+}
